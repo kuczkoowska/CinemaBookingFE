8d215342869817a66d1632309f8b6b1413ea525b
kuczkoowska <jagoda.kucz@wp.pl>
2025-12-14 19:37:23 +0100

    add home routing and view component

---
+++ src/app/core/home-routing/home-routing.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-home-routing',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './home-routing.component.html',
+})
+export class HomeRoutingComponent {
+
+}
---
+++ src/app/core/home-routing/views/home-view/home-view.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-home-view',
+  imports: [],
+  templateUrl: './home-view.component.html',
+})
+export class HomeViewComponent {
+
+}
