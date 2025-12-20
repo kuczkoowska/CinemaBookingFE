2c4e24c745562ae0f391c0741cee2a7f732b14b8
kuczkoowska <jagoda.kucz@wp.pl>
2025-12-20 18:20:46 +0100

    add movie list view routing and update component structure

--- src/app/core/movie-list-routing/movie-list-routing.component.ts
+++ src/app/core/movie-list-routing/movie-list-routing.component.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-movie-list-routing',
-  imports: [],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './movie-list-routing.component.html',
 })
 export class MovieListRoutingComponent {
--- src/app/core/movie-list-routing/movie.routes.ts
+++ src/app/core/movie-list-routing/movie.routes.ts
@@ -4,7 +4,12 @@ export default [
   {
     path: '',
     loadComponent: () => import('@cinemabooking/core/movie-list-routing/movie-list-routing.component').then((c) => c.MovieListRoutingComponent),
-    children: [],
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('@cinemabooking/core/movie-list-routing/views/movie-list-view/movie-list-view.component').then((c) => c.MovieListViewComponent),
+      }
+    ],
   },
 ] satisfies Route[];
 
