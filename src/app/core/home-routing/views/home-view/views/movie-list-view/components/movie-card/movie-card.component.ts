7fa0acbd488c63cb5d4dc0b9aeb1d472c78afd3c
kuczkoowska <jagoda.kucz@wp.pl>
2026-01-14 17:12:45 +0100

    refactor movie card component to use PrimeNG components and improve styling

--- src/app/core/home-routing/views/home-view/views/movie-list-view/components/movie-card/movie-card.component.ts
+++ src/app/core/home-routing/views/home-view/views/movie-list-view/components/movie-card/movie-card.component.ts
@@ -2,10 +2,14 @@ import {Component, input} from '@angular/core';
 import {Movie} from '@cinemabooking/interfaces/movie';
 import {GenreNamePipe} from '@cinemabooking/pipes/genre-name.pipe';
 import {RouterLink} from '@angular/router';
+import {Button} from 'primeng/button';
+import {Card} from 'primeng/card';
+import {PrimeTemplate} from 'primeng/api';
+import {Tag} from 'primeng/tag';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [GenreNamePipe, RouterLink],
+  imports: [GenreNamePipe, RouterLink, Button, Card, PrimeTemplate, Tag],
   templateUrl: './movie-card.component.html',
 })
 export class MovieCardComponent {
