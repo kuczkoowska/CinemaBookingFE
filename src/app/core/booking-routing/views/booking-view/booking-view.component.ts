d2770d67fcf56768993bb9f7d3a176153e6248ee
kuczkoowska <jagoda.kucz@wp.pl>
2026-01-14 17:46:51 +0100

    refactor booking view and movie card components to use PrimeNG elements and improve styling

--- src/app/core/booking-routing/views/booking-view/booking-view.component.ts
+++ src/app/core/booking-routing/views/booking-view/booking-view.component.ts
@@ -4,18 +4,20 @@ import {
 } from '@cinemabooking/core/booking-routing/views/booking-view/components/movie-card-booking/movie-card-booking.component';
 import {ActivatedRoute} from '@angular/router';
 import {bookingStore} from '@cinemabooking/stores/booking-store';
+import {ProgressSpinner} from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-booking-view',
   imports: [
     MovieCardBookingComponent,
+    ProgressSpinner,
   ],
   providers: [bookingStore],
   templateUrl: './booking-view.component.html',
 })
 export class BookingViewComponent implements OnInit {
-  protected store = inject(bookingStore);
-  private route = inject(ActivatedRoute);
+  protected readonly store = inject(bookingStore);
+  private readonly route = inject(ActivatedRoute);
 
   public ngOnInit(): void {
     const screeningId = Number(this.route.snapshot.paramMap.get('screeningId'));
--- src/app/core/booking-routing/views/booking-view/components/movie-card-booking/movie-card-booking.component.ts
+++ src/app/core/booking-routing/views/booking-view/components/movie-card-booking/movie-card-booking.component.ts
@@ -2,15 +2,23 @@ import {Component, input} from '@angular/core';
 import {Screening} from '@cinemabooking/interfaces/screening';
 import {Movie} from '@cinemabooking/interfaces/movie';
 import {DatePipe} from '@angular/common';
+import {Image} from 'primeng/image';
+import {Card} from 'primeng/card';
+import {Divider} from 'primeng/divider';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-movie-card-booking',
   imports: [
-    DatePipe
+    DatePipe,
+    Image,
+    Card,
+    Divider,
+    Button
   ],
   templateUrl: './movie-card-booking.component.html',
 })
 export class MovieCardBookingComponent {
-  public movie = input.required<Movie>();
-  public screening = input.required<Screening>();
+  public readonly movie = input.required<Movie>();
+  public readonly screening = input.required<Screening>();
 }
