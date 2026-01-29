488b550ed99419e17a5f17285ce8a98dfeffefba
kuczkoowska <jagoda.kucz@wp.pl>
2026-01-29 14:38:03 +0100

    add a button to change date and cancel booking

--- src/app/core/booking-routing/views/booking-view/booking-view.component.ts
+++ src/app/core/booking-routing/views/booking-view/booking-view.component.ts
@@ -2,9 +2,10 @@ import {Component, inject, OnInit} from '@angular/core';
 import {
   MovieCardBookingComponent
 } from '@cinemabooking/core/booking-routing/views/booking-view/components/movie-card-booking/movie-card-booking.component';
-import {ActivatedRoute, RouterOutlet} from '@angular/router';
-import {BookingStore} from '@cinemabooking/stores/booking-store';
+import {ActivatedRoute, Router, RouterOutlet} from '@angular/router';
+import {BookingStore} from '@cinemabooking/stores/booking.store';
 import {SpinnerComponent} from '@cinemabooking/ui/spinner/spinner.component';
+import {AppRoute} from '@cinemabooking/enums/app-routes';
 
 @Component({
   selector: 'app-booking-view',
@@ -20,9 +21,27 @@ import {SpinnerComponent} from '@cinemabooking/ui/spinner/spinner.component';
 export class BookingViewComponent implements OnInit {
   protected readonly store = inject(BookingStore);
   private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
 
   public ngOnInit(): void {
     const screeningId = Number(this.route.snapshot.paramMap.get('screeningId'));
     this.store.loadBookingData(screeningId);
+  }
+
+  // POPRAWIC
+  public cancelAndGoHome(): void {
+    const bookingId = this.store.activeBooking()?.id;
+    if (bookingId) {
+      this.store.cancelAndGoBack();
+    }
+    this.router.navigate([AppRoute.HOME]);
+  }
+
+  public changeScreening(): void {
+    const bookingId = this.store.activeBooking()?.id;
+    if (bookingId) {
+      this.store.cancelBookingSilent(bookingId);
+    }
+    this.router.navigate([AppRoute.MOVIES]);
   }
 }
--- src/app/core/booking-routing/views/booking-view/components/movie-card-booking/movie-card-booking.component.ts
+++ src/app/core/booking-routing/views/booking-view/components/movie-card-booking/movie-card-booking.component.ts
@@ -1,24 +1,23 @@
-import {Component, input} from '@angular/core';
-import {Screening} from '@cinemabooking/interfaces/screening';
-import {Movie} from '@cinemabooking/interfaces/movie';
-import {DatePipe} from '@angular/common';
-import {Image} from 'primeng/image';
-import {Card} from 'primeng/card';
-import {Divider} from 'primeng/divider';
-import {Button} from 'primeng/button';
+import { Component, input, output } from '@angular/core';
+import { Screening } from '@cinemabooking/interfaces/screening';
+import { Movie } from '@cinemabooking/interfaces/movie';
+import { DatePipe } from '@angular/common';
+import { Image } from 'primeng/image';
+import { Card } from 'primeng/card';
+import { Divider } from 'primeng/divider';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-movie-card-booking',
-  imports: [
-    DatePipe,
-    Image,
-    Card,
-    Divider,
-    Button
-  ],
+  imports: [DatePipe, Image, Card, Divider, Button],
   templateUrl: './movie-card-booking.component.html',
 })
 export class MovieCardBookingComponent {
   public readonly movie = input.required<Movie>();
   public readonly screening = input.required<Screening>();
+  public readonly changeDate = output<void>();
+
+  public onChangeDate(): void {
+    this.changeDate.emit();
+  }
 }
