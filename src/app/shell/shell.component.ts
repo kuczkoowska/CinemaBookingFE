085832d0f6ae19e3e478767be59dd7939f73b835
kuczkoowska <jagoda.kucz@wp.pl>
2026-01-30 22:46:56 +0100

    add toast notification component to shell

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NavbarComponent} from '@cinemabooking/shell/components/navbar/navbar.component';
+import {ToastModule} from 'primeng/toast';
 
 @Component({
   selector: 'app-shell',
   imports: [
     RouterOutlet,
-    NavbarComponent
+    NavbarComponent,
+    ToastModule
   ],
   templateUrl: './shell.component.html',
 })
