d0b2418ec348c4199601c1fff1572711f88c3247
kuczkoowska <jagoda.kucz@wp.pl>
2026-01-31 02:29:32 +0100

    move files

--- src/app/core/booking-routing/views/booking-view/views/screening-seats/components/seat/seat.component.ts
+++ src/app/core/booking-routing/views/booking-view/views/screening-seats/components/seat/seat.component.ts
@@ -2,7 +2,7 @@ import {Component, inject, input} from '@angular/core';
 import {Tooltip} from 'primeng/tooltip';
 import {NgClass} from '@angular/common';
 import {SeatWithStatus} from '@cinemabooking/interfaces/seat';
-import {BookingStore} from '@cinemabooking/stores/booking-store';
+import {BookingStore} from '@cinemabooking/stores/booking.store';
 
 @Component({
   selector: 'app-seat',
@@ -14,5 +14,5 @@ import {BookingStore} from '@cinemabooking/stores/booking-store';
 })
 export class SeatComponent {
   public store = inject(BookingStore);
-  public seat = input.required<SeatWithStatus>()
+  public seat = input.required<SeatWithStatus>();
 }
