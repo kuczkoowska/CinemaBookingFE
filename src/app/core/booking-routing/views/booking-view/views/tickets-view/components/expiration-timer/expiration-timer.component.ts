c6299afe9a7992a99634c43b79246b4af58ba248
kuczkoowska <jagoda.kucz@wp.pl>
2026-02-01 00:48:15 +0100

    expiration timer redirects to main page after it runs out

--- src/app/core/booking-routing/views/booking-view/views/tickets-view/components/expiration-timer/expiration-timer.component.ts
+++ src/app/core/booking-routing/views/booking-view/views/tickets-view/components/expiration-timer/expiration-timer.component.ts
@@ -1,28 +1,40 @@
-import {Component, inject} from '@angular/core';
+import {Component, effect, inject} from '@angular/core';
+import {Router} from '@angular/router';
 import {BookingStore} from '@cinemabooking/stores/booking.store';
-import {interval, map, Observable, startWith} from 'rxjs';
+import {interval, map, Observable, startWith, takeWhile} from 'rxjs';
 import {AsyncPipe, CommonModule} from '@angular/common';
-
+import {NotificationService} from '@cinemabooking/services/notification.service';
 
 import {MessageModule} from 'primeng/message';
 import {TagModule} from 'primeng/tag';
 
 @Component({
   selector: 'app-expiration-timer',
-  standalone: true,
-  imports: [
-    CommonModule,
-    AsyncPipe,
-    MessageModule,
-    TagModule
-  ],
+  imports: [CommonModule, AsyncPipe, MessageModule, TagModule],
   templateUrl: './expiration-timer.component.html',
 })
 export class ExpirationTimerComponent {
   readonly store = inject(BookingStore);
+  private readonly router = inject(Router);
+  private readonly notificationService = inject(NotificationService);
+
+  public constructor() {
+    effect((): void => {
+      if (this.store.isExpired() && !this.store.isFinished()) {
+        this.notificationService.showError(
+          'Czas minął',
+          'Czas na rezerwację minął. Przekierowywanie do repertuaru...',
+        );
+        setTimeout(() => {
+          this.router.navigate(['/showtimes']);
+        }, 2000);
+      }
+    });
+  }
 
   readonly timeLeft$: Observable<string> = interval(1000).pipe(
     startWith(0),
+    takeWhile(() => !this.store.isExpired(), true),
     map(() => {
       const expString = this.store.expirationTime();
       if (!expString) return '00:00';
