83a4ac30a11ac5a331b1a714868a63a6537da552
kuczkoowska <jagoda.kucz@wp.pl>
2026-02-01 01:03:00 +0100

    refactor movie card navigation

--- src/app/core/home-routing/views/home-view/views/movie-list-view/components/movie-card/movie-card.component.ts
+++ src/app/core/home-routing/views/home-view/views/movie-list-view/components/movie-card/movie-card.component.ts
@@ -1,17 +1,24 @@
-import {Component, input} from '@angular/core';
-import {Movie} from '@cinemabooking/interfaces/movie';
-import {GenreNamePipe} from '@cinemabooking/pipes/genre-name.pipe';
-import {RouterLink} from '@angular/router';
-import {Button} from 'primeng/button';
-import {Card} from 'primeng/card';
-import {PrimeTemplate} from 'primeng/api';
-import {Tag} from 'primeng/tag';
+import { Component, inject, input } from '@angular/core';
+import { Movie } from '@cinemabooking/interfaces/movie';
+import { GenreNamePipe } from '@cinemabooking/pipes/genre-name.pipe';
+import { Button } from 'primeng/button';
+import { Card } from 'primeng/card';
+import { PrimeTemplate } from 'primeng/api';
+import { Tag } from 'primeng/tag';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [GenreNamePipe, RouterLink, Button, Card, PrimeTemplate, Tag],
+  imports: [GenreNamePipe, Button, Card, PrimeTemplate, Tag],
   templateUrl: './movie-card.component.html',
 })
 export class MovieCardComponent {
+  private readonly router = inject(Router);
   public readonly movie = input.required<Movie>();
+
+  protected goToMovie(): void {
+    const id = this.movie().id;
+    if (id == null) return;
+    this.router.navigate(['movie', id]);
+  }
 }
